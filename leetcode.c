dc5698db96a37fec660df2cfda1901f0abdfab78
JRHU0048 <124759050+JRHU0048@users.noreply.github.com>
2023-12-09 13:41:19 +0800

    Update leetcode.c

--- leetcode.c
+++ leetcode.c
@@ -7,7 +7,7 @@ T100 相同的树
  *     struct TreeNode *right;
  * };
  */
-bool isSameTree(struct TreeNode* p, struct TreeNode* q) { 
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {      //始终要记住，你处理的对象是当前节点，有三种情况，分别讨论，然后直接递归左子树和右子树即可，注意力放在当前节点上！
     if (p == NULL && q == NULL) { 
         return true;
     } else if (p == NULL || q == NULL) {     //若p和q同时为NULL那么将在第一个if语句中执行true,所以本语句只能有一个为NULL
