42e7dfd7c3db1c9a8dcd489e1a7a7120c194702d
JRHU0048 <124759050+JRHU0048@users.noreply.github.com>
2023-12-11 00:47:55 +0800

    Update 粒子群算法cso.c

--- 粒子群算法cso.c
+++ 粒子群算法cso.c
@@ -291,3 +291,157 @@ int main() {
     return 0;
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+day1211
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define NP 100        // 种群规模
+#define D 30          // 维度
+#define G 2000        //迭代次数
+#define FI 0.5       //// 参数 ϕ
+
+double calculate_fitness(double position[]) {    //个体的计算适应度值
+    double sum = 0.0;
+    for (int i = 0; i < D; ++i) {
+        sum += position[i] * sin(sqrt(fabs(position[i])));
+    }
+    return  D * 418.98288727243369-sum;
+}
+
+double random_double() {    //生成0、1之间的随机数
+    return rand() / ((double)RAND_MAX + 1);
+}
+
+void initialize_aveparticles(double aveparticles[], double particles[][D]) {  //计算种群平均位置数组
+    for (int i = 0;i < D;++i)  //初始化aveparticles
+    {
+        aveparticles[i] = 0;
+    }
+    for (int j = 0;j < D;++j)  //在 C 中，在函数中声明二维数组参数时，必须指定至少一个维度的大小。在本例中， D 是第二个维度的大小（列数）。
+    {
+        for (int i = 0;i < NP;++i)
+        {
+            aveparticles[j] += particles[i][j];
+        }
+        aveparticles[j]  /=  NP;  //浮点数
+    }
+}
+
+int main() {
+    // 定义粒子群
+    double particles[NP][D];    //存储粒子位置信息
+    double velocities[NP][D];   //存储粒子速度信息
+    double aveparticles[D];     //存储平均位置信息
+    double best_fitness[G];       // 假设最大迭代次数为G,用来存储每一代的最佳适应度
+    double global_best_fitness = INFINITY;    //全局最佳适应度，初始化为正无穷大，可以确保在开始时，任何一个粒子的适应度值都能够成为当前的最佳适应度值。
+    double global_best_position[D];  //全局最优解的位置
+
+    srand((unsigned int)time(NULL));
+
+    // 初始化其他参数
+    int t = 0;
+    int U[NP];   //初始化尚未参加比赛的粒子集
+    for (int i = 0; i < NP; ++i) {
+        U[i] = i;   //未参与过的赋值i
+    }
+   
+    for (int i = 0;i < NP;++i)     //初始化位置信息
+    {
+        for (int j = 0;j < D;++j)
+        {
+            particles[i][j] = rand() % 1001 - 500;   //位置为 -500到500之间
+        }
+    }
+
+    for (int i = 0;i < NP;++i)   //初始化速度信息
+    {
+        for (int j = 0;j < D;++j)
+        {
+            velocities[i][j] = ((double)rand() / RAND_MAX) * 2.0 - 1.0;  //随机初始化速度为 -1.0到1.0之间
+        }
+    }
+
+    initialize_aveparticles(aveparticles, particles); //计算种群平均位置数组
+    
+    while (t < G) {  // 最大迭代次数
+        // 计算适应度
+        double fitness_values[NP];   //计算同一代中所有个体的适应度
+        for (int i = 0; i < NP; ++i) {
+            fitness_values[i] = calculate_fitness(particles[i]);  //particles[i]表示调用第i行，是一个一维数组
+        }
+
+        // 更新全局最优解以及最优位置
+        for (int i = 0; i < NP; ++i) {
+            if (fitness_values[i] < global_best_fitness) {
+                global_best_fitness = fitness_values[i];    //更新适应度为较小的值
+                for (int j = 0; j < D; ++j) {
+                    global_best_position[j] = particles[i][j];
+                }
+            }
+        }
+        // 记录每一代的最优解
+        best_fitness[t] = global_best_fitness;
+
+        // 选择两个粒子进行竞争
+        int idx1 = rand() % NP;
+        int idx2 = rand() % NP;
+        
+        /*do {
+            idx2 = rand() % NP;
+        } while (idx2 != idx1);*/
+
+        // 确定竞争结果
+        int winner, loser;
+        if (fitness_values[idx1] <= fitness_values[idx2]) {   //适应度较小的认为获胜
+            winner = idx1;
+            loser = idx2;
+        }
+        else {
+            winner = idx2;
+            loser = idx1;
+        }
+
+        // 使用学习策略更新失败粒子的速度
+        double R1[D], R2[D], R3[D];
+        for (int i = 0; i < D; ++i) {
+            R1[i] = random_double();   //D维随机数 0-1
+            R2[i] = random_double();
+            R3[i] = random_double();
+        }
+        for (int i = 0;i < D;++i) {
+            int vlt = velocities[loser][i];
+            velocities[loser][i] = R1[i] * vlt + R2[i] * (particles[winner][i] - particles[loser][i]) + FI*R3[i] * (aveparticles[i] - particles[loser][i]);
+            particles[loser][i] += velocities[loser][i];
+        }
+        // 更新U，将参与竞争的粒子移除
+        for (int i = 0; i < NP; ++i) {
+            if (U[i] == idx1 || U[i] == idx2) {
+                U[i] = -1;    //参与过竞争的赋值-1
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < NP; ++i) {
+            if (U[i] != -1) {      
+                U[count] = U[i];    //对于未参与过竞争的粒子放前面
+                count++;
+            }
+        }
+        //更新平均位置数组
+        initialize_aveparticles(aveparticles, particles); //计算种群平均位置数组
+        t++;
+    }
+
+    for (int i = 0;i < G;++i) {
+        printf("种群中第%d代的最佳适应度为：%lf\n", i,best_fitness[i]);
+    }
+    for (int i = 0;i < D;++i)
+    {
+        printf("%d, ", global_best_position);
+    }
+    system("pause");
+    return 0;
+}
